6d977140785bd8bfa5139488589608ae98a55274
jarehec <bone1198@gmail.com>
2017-06-10 00:59:07 +0000

    added started methods create, show

--- console.py
+++ console.py
@@ -6,6 +6,7 @@ import cmd
 class HBNBCommand(cmd.Cmd):
     'hbnb command interpreter'
     prompt = '(hbnb) '
+    classes = {'BaseModel': BaseModel, 'FileStorage': FileStorage}
 
     def do_quit(self, s):
         'Exits the shell'
@@ -14,6 +15,28 @@ class HBNBCommand(cmd.Cmd):
     def do_EOF(self, s):
         'Exits the shell'
         exit()
+
+    def do_create(self, name):
+        'create instance of basemodel, saves it'
+        if name is None:
+            print("** class doesn't exist **")
+        elif name is not "BaseModel":
+            print("** class name missing **")
+        else:
+            new_instance = name
+            print(new_instance.id)
+            FileStorage.save(new_instance)
+
+    def do_show(self, name, name_id):
+        'print string repr of an instance based on class name'
+        if name is None:
+            print("** no instance found **")
+        elif name_id is None:
+            print("** instance id missing **")
+        elif name.__class__ is None:
+            print("** class doesn't exist **")
+        else:
+            print(name)
 
     def emptyline(self):
         'emptyline method - does nothing'
