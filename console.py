a45022c6d5b5ea0bc28cec25cf9f2827907df11c
jarehec <bone1198@gmail.com>
2017-06-13 19:48:28 +0000

    completed tasks for 7

--- console.py
+++ console.py
@@ -29,24 +29,89 @@ class HBNBCommand(cmd.Cmd):
         else:
             new_instance = classes.get(args[0])()
             print(new_instance.id)
-            storage.save(new_instance)
+            storage.save()
 
     def do_show(self, args):
-        'print string repr of an instance based on class name'
+        'print string __str__ of an instance based on class name'
         args = args.split()
         'expecting: args = [name, name_id]'
         if len(args) != 2:
-        # if args[0] is None:
-            print("** class name missing **")
-        # elif args[1] is None:
             print("** instance id missing **")
         elif args[0] not in classes:
             print("** class doesn't exist **")
         else:
-            try:
-                print(args[1])
-            except:
-                print("++ no instance found ++")
+            for k, v in storage.all().items():
+                if args[1] == v.id:
+                    print(v)
+                    return
+            print("** no instance found **")
+
+    def do_all(self, args):
+        'print string __str__ of an instance based on class name or nothing'
+        args = args.split()
+        if not args:
+            for k, v in storage.all().items():
+                print(v)
+        elif args[0] not in classes:
+            print("** class doesn't exist **")
+        else:
+            for k, v in storage.all().items():
+                if args[0] == v.__class__.__name__:
+                    print(v)
+                    return
+            print('[]')
+                    
+
+    def do_update(self, args):
+        'updates an instance attribute based on class name and id'
+        args = args.split()
+        if not args:
+            print("** class name missing **")
+            return
+        elif len(args) < 2:
+            print("** instance id missing **")
+            return
+        elif len(args) < 3:
+            print("** attribute name missing **")
+            return
+        elif len(args) < 4:
+            print("** value missing **")
+            return
+        
+        if args[0] not in classes:
+            print("** class doesn't exist **")
+            return
+        for k, v in storage.all().items():
+            if args[1] == v.id:
+                args[3] = args[3].strip('"')
+                try:
+                    args[3] = int(args[3])
+                except:
+                    pass
+                setattr(v, args[2], args[3])
+                storage.save()
+                return
+            print("** no instance found **")
+
+
+    def do_destroy(self, args):
+        'deletes an instance based on class name or id'
+        args = args.split()
+        if not args:
+            print("** class name missing **")
+            return
+        elif len(args) < 2:
+            print("** instance id missing **")
+            return
+        if args[0] not in classes:
+            print("** class doesn't exist **")
+            return
+        for k, v in storage.all().items():
+            if args[1] == v.id:
+                del storage.all()[k]
+                storage.save()
+                return
+        print("** no instance found **")
 
     def emptyline(self):
         'emptyline method - does nothing'
