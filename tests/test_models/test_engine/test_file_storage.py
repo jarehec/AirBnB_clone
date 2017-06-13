fd51069bfecd3167dbba169edf11020061331e8d
KRLoyd <loydkristen@gmail.com>
2017-06-13 00:49:03 +0000

    Modify tests for __init__ and file_storage Create tests for console, amenity, city, place, review, state, and user

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,77 +1,46 @@
 #!/usr/bin/python3
-'Unittest for FileStorage class'
+"""Unittest for file_storage.py
+"""
 import unittest
 import uuid
-import json
-import models.engine.file_storage
-
-FileStorage = models.engine.file_storage.FileStorage
-all = FileStorage.all
-new = FileStorage.new
-save = __import__('file_storage').save
-reload = __import__('file_storage').reload
-
-class TestFileStorageDocs(unittest.Testcase):
-    'Tests for docs for FileStorage class'
-    def test_doc_file(self):
-        'Test: file documentation'
-        expected: 'File storage module'
-        actual: models.file_storage.__doc__
-        self.assertEqual(expected, actual)
-
-    def test_doc_class(self):
-        'Test: class documentation'
-        expected: 'FileStorage class'
-        actual: FileStorage.__doc__
-        self.assertEqual(expected, actual)
-
-    def test_dic_method_all(self):
-        'Test: method (all) documentation'
-        expected: 'Returns __objects'
-        actual: FileStorage.all.__doc__
-        self.assertEqual(expected, actual)
-
-    def test_dic_method_new(self):
-        'Test: method (new) documentation'
-        expected: 'adds an objects to the instance'
-        actual: FileStorage.new.__doc__
-        self.assertEqual(expected, actual)
-
-    def test_dic_method_save(self):
-        'Test: method (save) documentation'
-        expected: 'serializes __objects to JSON file'
-        actual: FileStorage.save.__doc__
-        self.assertEqual(expected, actual)
-
-    def test_dic_method_reload(self):
-        'Test: method (reload) documentation'
-        expected: 'deserializes the JSON file to __objects'
-        actual: FileStorage.reload.__doc__
-        self.assertEqual(expected, actual)
+import os
+from models import storage
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
 
 
-@classmethod
-def setUpClass(cls):
-    'Sets up __file_path and __objects for tests'
-    __file_path = "./file.json"
-    __objects = {}
-    
-
-class TestAll(unittest.TestCase):
-    'Tests all method from FileStorage'
-    def test_empty__object(self):
-        'all test: __object is an empty dictionary'
-        self.assert___(all(),)
-
-class TestNew(unittest.TestCase):
-    'Tests new method from FileStorage'
+class TestFileStorage(unittest.TestCase):
+    'class for testing FileStorage'
 
 
-class TestSave(unittest.TestCase):
-    'Tests save method from FileStorage'
+    def setUp(self):
+        'called multiple times, once before each test'
+        self.new_inst = FileStorage()
 
-class TestReload(unittest.TestCase):
-    'Tests reload method from FileStorage'
+    def tearDown(self):
+        'called multiple times, once after each test'
+        if os.path.exists("file.json"):
+            try:
+                os.remove("file.json")
+            except:
+                pass
+
+    def test_all(self):
+        'test correct return for all: __objects'
+        explicit = self.new_inst._FileStorage__objects
+        implicit = self.new_inst.all()
+        self.assertEqual(explicit, implicit)
+
+    def test_new(self):
+        'test that a new object is added to __objects'
+
+
+    def test_save(self):
+        'test that __object was serialized to JSON file'
+        
+
+    def test_reload(self):
+        'test that JSON file was deserialized to __objects'
 
 
 if __name__ == '__main__':
